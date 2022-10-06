6d77920a073256bc6ffd22fd4f5a3c864ec9bb2c
Vanessa O <vaneolipa@gmail.com>
2022-10-06 01:16:25 -0500

    config webpack for images

--- webpack.config.js
+++ webpack.config.js
@@ -27,6 +27,10 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
     ],
   },
  };
