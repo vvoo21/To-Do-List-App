49c7add8961214c626065ebd54e8f75e70ac310a
Vanessa O <vaneolipa@gmail.com>
2022-10-07 12:51:04 -0500

    Modify index.js

--- src/index.js
+++ src/index.js
@@ -1,62 +1,30 @@
 import './style.css';
+import DisplayTodos from './modules/functions.js';
 
-const todoList = document.querySelector('#todo-list-wrap');
+export let todos;
 
-const todos = [
-  {
-    content: 'Study JavaScript',
-    done: false,
-    id: 1,
-  },
+window.addEventListener('load', () => {
+  todos = JSON.parse(localStorage.getItem('todos')) || [];
+  const newTodoForm = document.querySelector('#new-todo-form');
 
-  {
-    content: 'Watch a video about localStorage',
-    done: false,
-    id: 2,
-  },
+  newTodoForm.addEventListener('submit', e => {
+    e.preventDefault();
 
-  {
-    content: 'Build a To Do List App',
-    done: false,
-    id: 3,
-  },
-];
+    const todo = {
+      content: e.target.elements.content.value,
+      done: false,
+      id: new Date().getTime()
+    }
 
-const DisplayTodos = () => {
-  todos.forEach((todo) => {
-    const todoItem = document.createElement('div');
-    todoItem.classList.add('todo-item');
+    //Global variable = todos
+    todos.push(todo);
 
-    const label = document.createElement('div');
-    const input = document.createElement('input');
-    const span = document.createElement('span');
-    const content = document.createElement('div');
-    const actions = document.createElement('div');
-    const editImg = document.createElement('img');
+    localStorage.setItem('todos', JSON.stringify(todos));
 
-    input.type = 'checkbox';
-    input.checked = todo.done;
-    input.setAttribute('aria-label', 'checkbox');
-    editImg.setAttribute('src', 'images/more.png');
-    editImg.setAttribute('alt', 'restart todo');
-    editImg.setAttribute('width', '20');
-    editImg.setAttribute('height', '20');
+    e.target.reset() 
 
-    label.classList.add('label');
-    span.classList.add('bubble');
-    content.classList.add('todo-content');
-    actions.classList.add('actions');
+    DisplayTodos();
+  })
 
-    content.innerHTML = `<input type="text" value="${todo.content}" aria-label="task" readonly>`;
-
-    todoList.appendChild(todoItem);
-    todoItem.appendChild(label);
-    todoItem.appendChild(content);
-    todoItem.appendChild(actions);
-    actions.appendChild(editImg);
-    label.appendChild(input);
-    label.appendChild(span);
-  });
-};
-
-DisplayTodos();
+  DisplayTodos();
+})
