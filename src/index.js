cc456ae223ab20a9195b6f1b36b03849f4fa822b
Vanessa O <vaneolipa@gmail.com>
2022-10-12 13:48:21 -0500

    Modify index.js

--- src/index.js
+++ src/index.js
@@ -1,27 +1,11 @@
 import './style.css';
 import DisplayTodos from './modules/functions.js';
+import addTodo from './modules/addTodo.js';
 
 window.addEventListener('load', () => {
-  const todos = JSON.parse(localStorage.getItem('todos')) || [];
   const newTodoForm = document.querySelector('#new-todo-form');
 
-  newTodoForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-
-    const todo = {
-      content: e.target.elements.content.value,
-      done: false,
-      id: todos.length + 1,
-    };
-
-    todos.push(todo);
-
-    localStorage.setItem('todos', JSON.stringify(todos));
-
-    e.target.reset();
-
-    DisplayTodos();
-  });
+  newTodoForm.addEventListener('submit', addTodo);
 
   DisplayTodos();
 });
